309925843c58b39e163a3dfb9b2cb3ccf4e5b6cc
Ilya Yudin <nibezo.cs@gmail.com>
2022-04-21 13:30:32 +0300

    feat: add learning/conspect/test place

---
+++ Conspect/Arrays operations/Arrays.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void main() {
+	// Create a new array 
+	// Show all array items
+	// Add new item
+	// Delete element from array
+	// Delete array
+	// Fill array randomly
+	// Get array's lenght
+	// Matrix example
+	// Dynamic array
+	// Show all dynamic array items
+	// Add new item dynamicly
+	// Get dynamic array lenght
+}
