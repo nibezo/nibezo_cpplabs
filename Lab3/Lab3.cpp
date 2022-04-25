8b3373297fbe73af447164ce799233a643e8ee23
Ilya Yudin <nibezo.cs@gmail.com>
2022-04-26 02:45:05 +0300

    lab3: feat: solve the 1st problem

--- Lab3/Lab3.cpp
+++ Lab3/Lab3.cpp
@@ -16,36 +16,59 @@
 #include <string>
 #include <stdlib.h>
 #include <sstream>
+#include <math.h>
+
+double nthRoot(int A, int N)
+{
+    // initially guessing a random number between
+    // 0 and 9
+    double xPre = rand() % 10;
+    //  smaller eps, denotes more accuracy
+    double eps = 1e-3;
+    // initializing difference between two
+    // roots by INT_MAX
+    double delX = INT_MAX;
+    //  xK denotes current value of x
+    double xK;
+    //  loop until we reach desired accuracy
+    while (delX > eps)
+    {
+        //  calculating current value from previous
+        // value by newton's method
+        xK = ((N - 1.0) * xPre +
+            (double)A / pow(xPre, N - 1)) / (double)N;
+        delX = abs(xK - xPre);
+        xPre = xK;
+    }
+    return xK;
+}
 
 using namespace std;
 int main() {
     fstream file;
-    int count = 0;
-    int num = 1;
-    int i = 0;
-    //string S = "-66";
-    //int N = stoi(S);
-    //cout << N << endl;
-
-    file.open("../files/f.txt", ios::in); //open a file to perform read operation using file object
-    if (file.is_open()) {   //checking whether the file is open
+    file.open("../files/f.txt", ios::in); 
+    //open a file to perform read operation using file object
+    if (file.is_open()) {   
+        //checking whether the file is open
+        int count = 0;
+        int product = 1;
+        int sum = 0;
         string line;
+        int odd = 0;
+        int even = 0;
+        int k = 0;
         cout << "In your file are the next numbers:" << endl;
         while (getline(file, line)) { 
             //read data from file object and put it into string.
             count++;
-            cout << line << endl;
         }
         file.close();
 
         cout << "Count of numbers is: " << count << endl;
-        
-        int* numbers = new int[count]; 
+        int* numbers = new int[count];
         //dinamic array with integer numbers
         file.open("../files/f.txt", ios::in);
-
         string line_2;
-        int k = 0;
         // type of the line from string to int
         while (getline(file, line_2)) {
             numbers[k] = stoi(line_2);
@@ -53,15 +76,32 @@ int main() {
         }
         file.close(); //close the file object.
         for (int i = 0; i < count; i++) {
-            cout << numbers[i] << endl;
+            if (numbers[i] % 2 == 0) {
+                product = product * numbers[i];
+                even++;
+            }
+            else {
+                sum = sum + numbers[i];
+                odd++;
+            }
         }
+        
+        // Calc arithmetic mean of odd numbers
+        double sum_a = (double)sum / odd;
+        // Calc geometric mean of even numbers
+        double product_g = (double)nthRoot(product, even);
+        
+        file.open("../files/arithmetic.txt", ios::out); 
+        if (file.is_open()) {
+            file << sum_a;   //inserting text
+            file.close();    //close the file object
+        }
+        file.open("../files/geometric.txt", ios::out); 
+        if (file.is_open()) {
+            file << product_g;   //inserting geometric
+            file.close();    //close the file object
+        }
+
         delete[] numbers;
     }
-    /*newfile.open("../files/f.txt", ios::out); // open a file to perform write operation using file object
-    if (newfile.is_open()) //checking whether the file is open
-    {
-        newfile << "10\n10\n";   //inserting text
-        newfile.close();    //close the file object
-    }
-    */
 }
